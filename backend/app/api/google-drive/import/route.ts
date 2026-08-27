0e10e8781a65dfde58300763c81a8b4abdfa009d
Chinmay Joshi <github.usercj@gmail.com>
2026-08-27 12:48:27 +0000

    Add Google Drive import route

---
+++ backend/app/api/google-drive/import/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+import { handleOptions } from '../../../../lib/cors';
+import { importDriveFile } from '../../../../services/googleDriveApi';
+
+export const runtime = 'nodejs';
+
+export async function OPTIONS(request: Request): Promise<Response> {
+  return handleOptions(request);
+}
+
+export async function POST(request: Request): Promise<Response> {
+  return importDriveFile(request);
+}
